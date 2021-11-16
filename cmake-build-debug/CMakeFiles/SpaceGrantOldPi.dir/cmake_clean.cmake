81b3f8112aaefc4fd25106b1012b5fdd517a4910
Shane Copenhagen <copenhagenmhs@gmail.com>
2021-11-15 18:36:33 -0700

    test cpp

---
+++ cmake-build-debug/CMakeFiles/SpaceGrantOldPi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceGrantOldPi.dir/main.cpp.obj"
+  "SpaceGrantOldPi.exe"
+  "SpaceGrantOldPi.exe.manifest"
+  "SpaceGrantOldPi.pdb"
+  "libSpaceGrantOldPi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpaceGrantOldPi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
