2f98f5e377ff2fda413db6a168ab5f56549d2c7f
colinbrush <97994812+colinbrush@users.noreply.github.com>
2022-01-27 17:18:36 -0700

    Add files via upload
    Should have basic moter control still needs to be tested on the pi

--- MotorControl.py
+++ MotorControl.py
@@ -30,10 +30,11 @@ GPIO.setup(ForwardLeft, GPIO.OUT)
 GPIO.setup(BackwardLeft, GPIO.OUT)
 GPIO.setup(RightPWM, GPIO.OUT)
 GPIO.setup(LeftPWM, GPIO.OUT)
-
+rightMotorPWM = GPIO.PWM(RightPWM, 1000)
+leftMotorPWM = GPIO.PWM(LeftPWM, 1000)
 
 # this is how you turn pin stuff  GPIO.output(pin, GPIO.HIGH)
-def RightMotor(seconds, direction, speed): # This controls left motor time is how long direction
+def rightMotor(direction, speed): # This controls left motor time is how long direction
     # is a boolean with true for forward and false for backward
     # Speed determines how fast
     if direction:
@@ -42,29 +43,53 @@ def RightMotor(seconds, direction, speed): # This controls left motor time is ho
     else:
         GPIO.output(ForwardRight, GPIO.LOW)
         GPIO.output(BackwardRight, GPIO.HIGH)
-    p = GPIO.PWM(RightPWM, 1000)
-    p.start(speed)
-    time.sleep(seconds)
-    p.stop()
+    rightMotorPWM.start(speed)
 
 
-def LeftMotor(seconds, direction, speed): #same as RightMotor but for LeftMotor
+# same as RightMotor but for LeftMotor
+def leftMotor(direction, speed):
     if direction:
         GPIO.output(ForwardLeft, GPIO.HIGH)
         GPIO.output(BackwardLeft, GPIO.LOW)
     else:
         GPIO.output(ForwardLeft, GPIO.LOW)
         GPIO.output(BackwardLeft, GPIO.HIGH)
-    p = GPIO.PWM(LeftPWM, 1000)
-    p.start(speed)
-    time.sleep(seconds)
-    p.stop()
+    leftMotorPWM.start(speed)
 
 
-print("forward motor ")
-LeftMotor(5, True, 50)
-print("reverse motor")
-RightMotor(5, False, 50)
-
-print("Stopping motor")
+print("Enter control as \"command speed time \"")
+check = True
+while check:
+    control = input("Enter control")
+    if control.lower() == "stop":
+        check = False
+    elif control.lower() == "help":
+        print("This is help")
+    else:
+        commands = control.split(" ")
+        try:
+            speed = commands[1]
+        except:
+            print("you suck")
+            continue
+        try:
+            waiting = commands[2]
+        except:
+            print("you suck")
+            continue
+        if commands[0].lower() == "forward":
+            rightMotor(True, speed)
+            leftMotor(True, speed)
+        elif commands[0].lower() == "back":
+            rightMotor(False, speed)
+            leftMotor(False, speed)
+        elif commands[0].lower() == "right":
+            rightMotor(False, speed)
+            leftMotor(True, speed)
+        elif commands[0].lower() == "left":
+            rightMotor(True, speed)
+            leftMotor(True, speed)
+        time.sleep(waiting)
+        rightMotorPWM.stop()
+        leftMotorPWM.stop()
 GPIO.cleanup()
